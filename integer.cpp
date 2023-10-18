341f49411d88e634c7f37fc90e4576ead158b306
Lily Carroll <lily.carroll@stu.ocsb.ca>
2023-10-18 21:36:53 +0000

    corrected integer.cpp

---
+++ integer.cpp
@@ -0,0 +1,28 @@
+// Copyright (c) 2021 Lily Carroll All rights reserved.
+//
+// Created by: Lily Carroll
+// Created on: Oct/13/2023
+// This program asks the user for a number
+// and tells them if their number is
+// positive, negative or 0.
+#include <iostream>
+
+int main() {
+    // Declaring variable for the user's number.
+    int userNumber;
+
+    // Getting user input.
+    std::cout << "Enter an integer: ";
+    std::cin >> userNumber;
+
+    // Using if condition for positive integers.
+    // Using Else If condition for negative integers.
+    // Using Else condition for the integer 0.
+    if (userNumber > 0) {
+        std::cout << "Your number is positive.";
+    } else if (userNumber < 0) {
+        std::cout << "Your number is negative.";
+    } else {
+        std::cout << "Your number is 0.";
+    }
+}
